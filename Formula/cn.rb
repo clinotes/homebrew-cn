52f356ef388af842f526ef67bdef675870577d2d
Sebastian Müller <mail@sbstjn.com>
2016-11-18 20:33:12 +0100

    Update cn.rb

--- Formula/cn.rb
+++ Formula/cn.rb
@@ -2,8 +2,8 @@ class Cn < Formula
   desc "CLInotes client application"
   homepage "https://github.com/clinotes/client"
   url "https://dl.clinot.es/latest/cn_latest_darwin_amd64.zip"
-  version "0.2.0"
-  sha256 "e34a7b77e23b70f6e7bdc98f9ef0202078761712109ea8772e69d71b0dbd5dfe"
+  version "0.2.1"
+  sha256 "7184ec3ea8050e77a67fd37ce562fc9e4eb13c16086ba6cda8221f9848c88c91"
 
   def install
     bin.install "cn"
